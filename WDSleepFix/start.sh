2c6acf35de2f6611bee96b107a5ed8834307a7b8
André Ferreira <53491595+thebadking@users.noreply.github.com>
2020-01-28 09:26:17 +0100

    Update start.sh

--- WDSleepFix/start.sh
+++ WDSleepFix/start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-ACTION="stop"
-/etc/init.d/atop $ACTION 2>&1 >/dev/null &
-/etc/init.d/cmmgrd $ACTION 2>&1 >/dev/null &
-/etc/init.d/wdmcserverd $ACTION 2>&1 >/dev/null &
-/etc/init.d/wdphotodbmergerd $ACTION 2>&1 >/dev/null &
-/etc/init.d/onbrdnetloccommd $ACTION 2>&1 >/dev/null &
-/etc/init.d/wdnotifierd $ACTION 2>&1 >/dev/null &
-/etc/init.d/wddispatcherd $ACTION 2>&1 >/dev/null &
-#/etc/init.d/restsdk-served $ACTION 2>&1 >/dev/null &
+/etc/init.d/atop stop 2>&1 >/dev/null &
+/etc/init.d/commgrd stop 2>&1 >/dev/null &
+/etc/init.d/onbrdnetloccommd stop 2>&1 >/dev/null &
+/etc/init.d/restsdk-serverd stop 2>&1 >/dev/null &
+/etc/init.d/wddispatcherd stop 2>&1 >/dev/null &
+/etc/init.d/wdmcserverd stop 2>&1 >/dev/null &
+/etc/init.d/wdnotifierd stop 2>&1 >/dev/null &
+/etc/init.d/wdphotodbmergerd stop 2>&1 >/dev/null &
 killall crond 2>&1 >/dev/null &
 mount -o remount,noatime,nodiratime /dev/root /
